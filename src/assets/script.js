c9c62ad894834cb444242050b9a1f58580ae174d
Ben Ghaem <ben.ghaem@gmail.com>
2016-02-13 19:42:24 -0500

    first working version with lmb and rmb as well as mouse enable/disable

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,20 +1,16 @@
 var ws = new WebSocket("ws:10.0.1.150:9000")
-var old_pos = 0
-var old_delta = 0
-var prev_acc = 0
-var prev_vel = 0
+var old_pos = [0,0,0]
+var prev_acc = [0,0,0]
+var prev_vel = [0,0,0]
 var last_tick = Date.now()
 var current_tick = Date.now()
 
-function push(x, delta){
-    var new_vel = ((delta*((x + prev_acc)/2)) + prev_vel) / 10
-    document.getElementById('x-v').innerHTML = new_vel
-    var new_pos = ((delta) * ((new_vel + prev_vel) / 2) + old_pos)
-    document.getElementById('x-p').innerHTML = new_pos
-    ws.send(new_pos - old_pos)
-    old_pos = new_pos
-    prev_vel = new_vel
-    old_delta = delta
+function push(val, delta, index){
+    var new_vel = ((delta*((val + prev_acc[index])/2)) + prev_vel[index]) / 10
+    var new_pos = ((delta) * ((new_vel + prev_vel[index]) / 2) + old_pos[index])
+    ws.send(index + " " + (new_pos - old_pos[index]))
+    old_pos[index] = new_pos
+    prev_vel[index] = new_vel
 }
 
 function pretty_print(x){
@@ -30,18 +26,51 @@ window.addEventListener('devicemotion',function(event){
     last_tick = current_tick
     current_tick = Date.now()
 
-    x = event.accelerationIncludingGravity.x - 0.0655
-    //x = (event.rotationRate.gamma + 0.05)* -1
+    //x = event.accelerationIncludingGravity.x - 0.0655
+    //y = event.accelerationIncludingGravity.z - 0.06 +9.8
+    x = (event.rotationRate.gamma + 0.05)* -0.15
+    y = ( event.rotationRate.alpha) * -0.1
+    //y = (y - 0.2) * 100
+    //x = x * 50
+
 
     if (x < 0.01 && x > -0.02 ){
          x = 0
     }
+    if (y < 0.02 && y > -0.02 ){
+         y = 0
+    }
 
-    document.getElementById('x').innerHTML = x
-
-
-    push(x, current_tick - last_tick)
+    push(x, current_tick - last_tick, 0)
+    push(y, current_tick - last_tick, 1)
     //ws.send(pretty_print(x))
     //ws.send(event.acceleration.y)
     //ws.send(event.acceleration.z)
 })
+
+$(function(){
+    $("#lmb").on("pointerdown",function() {
+        ws.send("6d")
+    })
+    $("#lmb").on("pointerup",function() {
+        ws.send("6u")
+    })
+
+    $("#rmb").on("pointerdown",function() {
+        ws.send("7d")
+    })
+    $("#rmb").on("pointerup", function() {
+        ws.send("7u")
+    })
+    $("#release").on("pointerdown",function() {
+        ws.send("9d")
+    })
+    $("#release").on("pointerup", function() {
+        ws.send("9u")
+    })
+
+})
+
+//Disable scroll
+document.ontouchstart = function(e){ e.preventDefault(); }
+
--- src/wsmouse.cpp
+++ src/wsmouse.cpp
@@ -16,12 +16,48 @@ using websocketpp::lib::bind;
 
 typedef websocketpp::server<websocketpp::config::asio> server;
 
-void on_message(Display *disp, server *s, websocketpp::connection_hdl, server::message_ptr msg) {
-        std::cout << msg->get_payload() << std::endl;
-        int x = atoi(msg->get_payload().c_str());
-        XTestFakeRelativeMotionEvent(disp, x, 0, CurrentTime);
-        XFlush(disp);
+void on_message(Display *disp, server *s, bool *acc_on, websocketpp::connection_hdl, server::message_ptr msg) {
+        //std::cout << msg->get_payload() << std::endl;
+        std::string base = msg->get_payload();
+        int msgid= atoi(base.substr(0,1).c_str());
+        //std::cout << base.substr(0,1) << ":" << msgid << std::endl;
+        int value = 0;
+        bool down = false;
+        if (msgid >=0 && msgid < 6){
+            value = atoi(base.substr(2).c_str());
+        }
+        if (msgid >= 6 && msgid <= 9){
+           // std::cout << base.substr(1) << std::endl;
+            down = (base[1] == 'd');
+            if (down){
+                std::cout << "mousedown" <<std::endl;
+            } else {
+                std::cout << "mouseup" << std::endl;
+            }
+        }
+        //std::cout << msgid << ": " << value << std::endl;
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        if (msgid == 0){
+            x = value;
+        } else if (msgid == 1){
+            y = value;
+        } else if (msgid == 2){
+            z = value;
+        } else if (msgid == 6){
+            XTestFakeButtonEvent(disp, 1, down, CurrentTime);
+        } else if (msgid == 7){
+            XTestFakeButtonEvent(disp, 3, down, CurrentTime);
+        } else if (msgid == 9){
+            *acc_on = !down;
+        }
+        if (*acc_on){
+            XTestFakeRelativeMotionEvent(disp, x, y, CurrentTime);
+            XFlush(disp);
+        }
 }
+
 
 int main(){
 
@@ -31,9 +67,11 @@ int main(){
             exit(1);
         }
 
+    bool acc_on = true;
+
     server wsmouse_server;
 
-    wsmouse_server.set_message_handler(bind(&on_message, disp, &wsmouse_server, ::_1, ::_2));
+    wsmouse_server.set_message_handler(bind(&on_message, disp, &wsmouse_server, &acc_on, ::_1, ::_2));
     wsmouse_server.init_asio();
     wsmouse_server.listen(9000);
     wsmouse_server.start_accept();
